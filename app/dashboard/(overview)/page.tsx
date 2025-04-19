56907a3f671906d255a0d2201579873f7f2ec771
Avanish Tiwari <avanishtiwari@outlook.in>
2025-04-19 23:27:05 +0530

    deleted cmnted

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,6 @@
-// import { Card } from "@/app/ui/dashboard/cards"
 import RevenueChart from "@/app/ui/dashboard/revenue-chart"
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
 import { lusitana } from "@/app/ui/fonts"
-// import { fetchCardData } from "../../lib/data"
 import { Suspense } from "react"
 import CardWrapper from '@/app/ui/dashboard/cards';
 
@@ -13,8 +11,6 @@ import {
 } from "@/app/ui/skeletons"
 
 export default async function Page() {
-  // const revenue=await fetchRevenue()
-  // const latestInvoices= await fetchLatestInvoices()
 
 
   return (
